892961cced1355acee1a2c88f7e7dbd63eeb9482
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-02-23 17:35:31 +0100

    Update object.c
    Split the object_function into objects/object_function.c

--- src/object.c
+++ src/object.c
@@ -2,73 +2,8 @@
  * Project: Doctra
  */
 #include <stdlib.h>
-#include <string.h>
 #include "strlist.h"
-#include "object.h"
-
-// DOC_FUNCTION
-
-/**
- * function_init()
- * @self - The structure itself.
- * @name - The name of the function.
- *
- * Initialize's a doc_function structure.
- *
- * Return: none
- */
-void
-function_init (struct doc_function *self, char *name)
-{
-	self->name = name;
-	self->description = strdup ("");
-	self->returns = NULL;
-	
-	// Parameter
-	self->args_amnt = 0;
-	self->args[DOC_ARGS_NAME] = NULL;
-	self->args[DOC_ARGS_DESC] = NULL;
-}
-
-/**
- * function_free()
- * @self - The structure itself.
- *
- * Free's the allocated memory of a function structure.
- *
- * Return: none
- */
-void
-function_free (struct doc_function *self)
-{
-	free (self->name);
-	free (self->description);
-	free (self->returns);
-	
-	strlist_free (self->args, DOC_ARGS_LAST, self->args_amnt);
-}
-
-/**
- * function_arg_insert()
- * @self - The function object itself.
- * @name - Name of the argument.
- * @description - Description of the argument.
- *
- * Insert a new argument/parameter to the function.
- *
- * Return: none
- */
-void
-function_arg_insert (struct doc_function *self, char *name, char *description)
-{
-	self->args[DOC_ARGS_NAME] = strlist_insert (self->args[DOC_ARGS_NAME],
-							name, ++self->args_amnt);
-						    
-	self->args[DOC_ARGS_DESC] = strlist_insert (self->args[DOC_ARGS_DESC],
-							description, self->args_amnt); 
-}
-
-// DOC_OBJECT
+#include "objects.h"
 
 /**
  * object_append()

fdda24e03685b50f67a021b32a02dba166aabcea
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-02-24 22:28:52 +0100

    Implement the struct object.

--- src/exports/export_md.c
+++ src/exports/export_md.c
@@ -53,6 +53,47 @@ export_md_function (struct doc_function *self, FILE *f_doc)
 }
 
 /**
+ * export_md_struct()
+ * @self - The struct data.
+ * @f_doc - Stream to which the data gets written.
+ *
+ * Exports a struct object to the file @f_doc
+ * in Markdown.
+ *
+ * Return: none
+ */
+static void
+export_md_struct (struct doc_struct *self, FILE *f_doc)
+{
+	// Name
+	fputs (self->name, f_doc);
+	fputs ("\n", f_doc);
+	
+	// Underline
+	for (size_t i = 0; i < strlen (self->name); i++)
+	{
+		fputs ("=", f_doc);
+	}
+	fputs ("\n", f_doc);
+	
+	// Description
+	fputs (self->description, f_doc);
+	fputs ("\n", f_doc);
+	
+	// Members
+	fputs ("\n**Members**\n\n", f_doc);
+	fputs ("**Name** | **Description**\n", f_doc);
+	fputs ("-------- | ---------------\n", f_doc);
+	
+	for (size_t i = 0; i < self->members_amnt; i++)
+	{
+		fputs (self->members[STRUCT_MEMBERS_NAME][i], f_doc);
+		fputs (" | ", f_doc);
+		fputs (self->members[STRUCT_MEMBERS_DESC][i], f_doc);
+	}
+}
+
+/**
  * export_md()
  * @objs - The list head.
  * @filename - The name of the documentation file.
@@ -77,6 +118,9 @@ export_md (struct doc_object *objs, const char *filename)
 		{
 			case DOC_ELEMENT_FUNCTION:
 				export_md_function (&next->element.func, f_doc);
+				break;
+			case DOC_ELEMENT_STRUCT:
+				export_md_struct (&next->element.struc, f_doc);
 				break;
 			default:
 				break;

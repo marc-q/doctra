c1b661ef93bb3525af4f1d9edff306d7e655119a
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-02-25 21:26:54 +0100

    Update folder "src/exports"
    Rewrite number 2.
    New structure that fits the needs of doctra better.

--- src/exports/export_md.c
+++ src/exports/export_md.c
@@ -3,8 +3,36 @@
  */
 #include <stdio.h>
 #include <string.h>
-#include "../objects.h"
+#include "../object.h"
+#include "../node.h"
 #include "export_md.h"
+
+/**
+ * export_md_underline()
+ * @str - String to output with an underline.
+ * @fill - String to output as underline.
+ * @f_doc - Stream to which the output gets written.
+ *
+ * Writes @str with a underline made of @fill
+ * to the @f_doc stream.
+ *
+ * Return: none
+ */
+static void
+export_md_underline (const char *str, const char *fill, FILE *f_doc)
+{
+	// Output the string
+	fputs (str, f_doc);
+	fputs ("\n", f_doc);
+	
+	const size_t str_len = strlen (str);
+	// Output the underline
+	for (size_t i = 0; i < str_len; i++)
+	{
+		fputs (fill, f_doc);
+	}
+	fputs ("\n", f_doc);
+}
 
 /**
  * export_md_function()
@@ -17,39 +45,28 @@
  * Return: none
  */
 static void
-export_md_function (struct doc_function *self, FILE *f_doc)
-{
-	// Function name
-	fputs (self->name, f_doc);
-	fputs ("\n", f_doc);
-	
-	// Underline
-	for (size_t i = 0; i < strlen (self->name); i++)
-	{
-		fputs ("=", f_doc);
-	}
-	fputs ("\n", f_doc);
-	
-	// Function description
+export_md_function (struct doc_object *self, FILE *f_doc)
+{	
+	// Description
 	fputs (self->description, f_doc);
-	fputs ("\n", f_doc);
 	
 	// Parameters
 	fputs ("\n**Parameters**\n\n", f_doc);
 	fputs ("**Name** | **Description**\n", f_doc);
 	fputs ("-------- | ---------------\n", f_doc);
 	
-	for (size_t i = 0; i < self->args_amnt; i++)
+	for (size_t i = 0; i < self->members_amnt; i++)
 	{
-		fputs (self->args[FUNCTION_ARGS_NAME][i], f_doc);
+		fputs (self->members[OBJ_MEMBERS_NAME][i], f_doc);
 		fputs (" | ", f_doc);
-		fputs (self->args[FUNCTION_ARGS_DESC][i], f_doc);
+		fputs (self->members[OBJ_MEMBERS_DESC][i], f_doc);
+		fputc ('\n', f_doc);
 	}
 	
 	// Returns
 	fputs ("\n**Returns**\n", f_doc);
 	fputs (self->returns, f_doc);
-	fputs ("\n", f_doc);
+	fputs ("\n\n", f_doc);
 }
 
 /**
@@ -63,21 +80,10 @@ export_md_function (struct doc_function *self, FILE *f_doc)
  * Return: none
  */
 static void
-export_md_struct (struct doc_struct *self, FILE *f_doc)
-{
-	// Name
-	fputs (self->name, f_doc);
-	
-	// Underline
-	for (size_t i = 0; i < strlen (self->name); i++)
-	{
-		fputs ("=", f_doc);
-	}
-	fputs ("\n", f_doc);
-	
+export_md_struct (struct doc_object *self, FILE *f_doc)
+{	
 	// Description
 	fputs (self->description, f_doc);
-	fputs ("\n", f_doc);
 	
 	// Members
 	fputs ("\n**Members**\n\n", f_doc);
@@ -86,23 +92,24 @@ export_md_struct (struct doc_struct *self, FILE *f_doc)
 	
 	for (size_t i = 0; i < self->members_amnt; i++)
 	{
-		fputs (self->members[STRUCT_MEMBERS_NAME][i], f_doc);
+		fputs (self->members[OBJ_MEMBERS_NAME][i], f_doc);
 		fputs (" | ", f_doc);
-		fputs (self->members[STRUCT_MEMBERS_DESC][i], f_doc);
+		fputs (self->members[OBJ_MEMBERS_DESC][i], f_doc);
+		fputc ('\n', f_doc);
 	}
 }
 
 /**
  * export_md()
- * @objs - The list head.
+ * @nodes - The list head.
  * @filename - The name of the documentation file.
  *
- * Exports the objects in Markdown to a file named @filename.
+ * Exports the @nodes in Markdown to a file named @filename.
  *
  * Return: none
  */
 void
-export_md (struct doc_object *objs, const char *filename)
+export_md (struct doc_node *nodes, const char *filename)
 {
 	FILE *f_doc = fopen (filename, "w");
 	if (f_doc == NULL)
@@ -110,16 +117,19 @@ export_md (struct doc_object *objs, const char *filename)
 		return;
 	}
 	
-	struct doc_object *next = objs;
+	struct doc_node *next = nodes;
 	while (next)
 	{
+		// The name of the object with an underline
+		export_md_underline (next->name, "=", f_doc);
+		
 		switch (next->type)
 		{
-			case DOC_ELEMENT_FUNCTION:
-				export_md_function (&next->element.func, f_doc);
+			case DOC_NODE_FUNCTION:
+				export_md_function (&next->element, f_doc);
 				break;
-			case DOC_ELEMENT_STRUCT:
-				export_md_struct (&next->element.struc, f_doc);
+			case DOC_NODE_STRUCT:
+				export_md_struct (&next->element, f_doc);
 				break;
 			default:
 				break;
--- src/exports/export_md.h
+++ src/exports/export_md.h
@@ -1,6 +1,6 @@
 #ifndef __DOCTRA_EXPORT_MD_H__
 #define __DOCTRA_EXPORT_MD_H__
 
-void export_md (struct doc_object*, const char*);
+void export_md (struct doc_node*, const char*);
 
 #endif /* __DOCTRA_EXPORT_MD_H__ */

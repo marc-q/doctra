90801aecf08aff3698c97276bcbbfd265c4b526e
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-04-22 14:11:50 +0200

    Move since it should not be indexed on the web. (#52)
    * Move since it should not be indexed on the web.
    
    * Update and rename doc/makedoc.sh to makedoc.sh
    
    * Update README.md

--- makedoc.sh
+++ makedoc.sh
@@ -1,42 +1,43 @@
 #!/bin/sh
 
-doctrabin="../doctra"
+doctrabin="./doctra"
+docs="docs"
 # Build the header documentation first
 # because they are placed on top of the
 # page.
-for file in ../src/*.h
+for file in src/*.h
 do
 	echo "Building header documentation for $file"
 	fname=$(basename "$file" ".h")
-	$doctrabin "$file" "$fname.md"
+	$doctrabin "$file" "$docs/$fname.md"
 done
 
-for file in ../src/*/*.h
+for file in src/*/*.h
 do
 	echo "Building header documentation for $file"
 	fname=$(basename "$file" ".h")
-	$doctrabin "$file" "$fname.md"
+	$doctrabin "$file" "$docs/$fname.md"
 done
 
 # Build the source documentation after
 # the headers because they are placed
 # under them.
-for file in ../src/*.c
+for file in src/*.c
 do
 	echo "Appending source documentation for $file"
 	fname=$(basename "$file" ".c")
 	$doctrabin "$file" "tmp.md"
-	echo "\n" >> "$fname.md"
-	cat "tmp.md" >> "$fname.md"
+	echo "\n" >> "$docs/$fname.md"
+	cat "tmp.md" >> "$docs/$fname.md"
 done
 
-for file in ../src/*/*.c
+for file in src/*/*.c
 do
 	echo "Appending source documentation for $file"
 	fname=$(basename "$file" ".c")
 	$doctrabin "$file" "tmp.md"
-	echo "\n" >> "$fname.md"
-	cat "tmp.md" >> "$fname.md"
+	echo "\n" >> "$docs/$fname.md"
+	cat "tmp.md" >> "$docs/$fname.md"
 done
 
 rm "tmp.md"
